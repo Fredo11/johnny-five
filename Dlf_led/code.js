34ed721a9a8b19beba9ba7a21ba750363ab17deb
dlefur <dlefur@gmail.com>
2017-12-26 10:13:04 -0200

    Dlf_led folder

---
+++ Dlf_led/code.js
@@ -0,0 +1,39 @@
+//Initialisation du socket
+var socket = io.connect();
+
+socket.on('news', function (data) {
+	console.log(data);
+});
+
+
+//Variables des composants
+var boutonOn=document.getElementById('boutonOn');
+var boutonOff=document.getElementById('boutonOff');
+
+//Configuration initiale des boutons
+boutonOff.disabled=true;
+boutonOn.focus();
+
+
+//Évément du boutonOn
+boutonOn.addEventListener('click',function(event){
+	console.log('LED ON');
+	socket.emit('led:on');
+	boutonOn.disabled=true;
+	boutonOff.disabled=false;
+	boutonOff.focus();
+	event.preventDefault(); //évite l'action par défaut.
+	event.stopPropagation(); // stoppe la propagation de l'événement.
+});
+
+
+//Évément du boutonOff
+boutonOff.addEventListener('click',function(event){
+	console.log('LED OFF');
+	socket.emit('led:off');
+	boutonOff.disabled=true;
+	boutonOn.disabled=false;
+	boutonOn.focus();
+	event.preventDefault(); //évite l'action par défaut.
+	event.stopPropagation(); // stoppe la propagation de l'événement.
+});
---
+++ Dlf_led/server.js
@@ -0,0 +1,62 @@
+// server.js
+//////////////////////////////////////////////////////
+var express        = require('express');  
+var app            = express();  
+var httpServer = require("http").createServer(app);  
+var five = require("johnny-five");  
+var io=require('socket.io')(httpServer);
+ 
+var port = 3000; 
+ 
+app.use(express.static(__dirname));
+ 
+app.get('/', function(req, res) {  
+        res.sendFile(__dirname + '/index.html');
+});
+ 
+httpServer.listen(port);  
+console.log('Server available at http://localhost:' + port);  
+
+//////////////////////////////////////////////////////
+
+
+var led;
+ 
+
+ var board = new five.Board(); 
+
+//A la comnnection d'Arduino 
+board.on("ready", function() {  
+    console.log('Arduino connected');
+    led = new five.Led(2); // une led branchée sur D2
+});
+
+
+//A la déconnexion d'Arduino
+board.on("exit", function() {  
+    console.log('Arduino disconnected');
+    led.off();
+});
+ 
+//Socket connection handler
+io.on('connection', function (socket) {  
+        console.log("socket : " + socket.id);
+        
+        //Allumage de la led
+        socket.on('led:on', function (data) {
+           led.on();
+           console.log('LED ON RECEIVED');
+        });
+ 
+        //Éteignement de la led
+        socket.on('led:off', function (data) {
+            led.off();
+            console.log('LED OFF RECEIVED');
+ 
+        });
+    });
+
+//Message d'attente 
+console.log('Waiting for connection');
+ 
+ 
